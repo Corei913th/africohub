8d9b5b3f642cc8070a9e6256ae17cfb714b91d65
Corei913th <bogningfredy972@gmail.com>
2026-05-13 11:36:02 +0100

    chore(hooks): ajout des hooks pre-commit et commit-msg versionnés

---
+++ exercices.js
@@ -0,0 +1,9 @@
+// WIP 1
+// WIP 2
+
+// Module exercices Python
+const exercices = [
+  { id: 1, titre: 'Variables et types', langage: 'Python' },
+  { id: 2, titre: 'Boucles et conditions', langage: 'Python' }
+];
+module.exports = exercices;
